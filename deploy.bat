9a9ddc18a0e9860b3798dce4725c9c4a7e6d9469
chunibyo-wly <359806334@qq.com>
2020-02-05 22:11:31 +0800

    2020/02/05 周三

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,13 @@
+color 16
+
+echo deploy blog
 call hexo clean
 call hexo g
 call hexo d
+
+echo 开始提交代码
+git add -A .
+git commit -m "%date%"
+git push
+echo 完成
+
---
+++ source/_posts/data-utils.md
@@ -0,0 +1,9 @@
+---
+typora-root-url: ..
+title: data_utils
+mathjax: true
+date: 2020-02-05 22:05:54
+tags:
+categories:
+---
+<!-- more -->
